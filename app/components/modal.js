b6acc3c925e712bc755146f658f953ae2681fcb5
DANNNN <danielemerald@ymail.com>
2026-04-02 21:16:45 +0700

    Update modal.js

--- app/components/modal.js
+++ app/components/modal.js
@@ -8,7 +8,7 @@ export default function Modal({ title, description, children, setIsOpen }) {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.15 }}
-      className="fixed top-0 left-0 bg-black/85 w-full h-full z-100 px-6 md:px-12 flex flex-col items-center justify-center"
+      className="fixed top-0 left-0 bg-black/50 w-full h-full z-100 px-6 md:px-12 flex flex-col items-center justify-center"
     >
       <div className="card w-full max-w-[560px] flex flex-col gap-5">
         <div className="flex items-center justify-between">
